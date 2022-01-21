5615cb802e541a3ac9b0035f31507fe8bf45abfc
sumukshashidhar <sumukuuu@gmail.com>
2022-01-21 11:57:47 -0600

    feat[keep]: fix linting and test cases

--- src/keep.py
+++ src/keep.py
@@ -1,7 +1,34 @@
 """Simple module that should pass the pylint testcase for sure."""
+
+
 class Keep:
+    """Simple test class to test out basic Git workflows."""
+
     def __init__(self) -> None:
-        pass
-    
-    def say_hello(self):
-        return "Hello World"
+        """Initialize the class"""
+        self.message = ""
+        self.first_number = 0
+        self.second_number = 0
+
+    def say_hello(self, message):
+        """Says Hello to the function caller
+
+        Returns:
+            str: The hello world message
+        """
+        self.message = message
+        return "Hello" + message
+
+    def add_numbers(self, first_number, second_number):
+        """Adds two numbers
+
+        Args:
+            a (Int): The first number
+            b (Int): The second number
+
+        Returns:
+            Int: The sum of the two numbers
+        """
+        self.first_number = first_number
+        self.second_number = second_number
+        return first_number + second_number
--- src/test_keep.py
+++ src/test_keep.py
@@ -1,8 +1,27 @@
+"""Simple unittest test case to test the basic keep file."""
 import unittest
 import keep
+
+
 class TestHello(unittest.TestCase):
+    """Tests the keep module"""
 
     def test_hello(self):
+        """Test the hello message function
+
+        Returns:
+            None: Returns nothing.
+        """
         # make a new keep object
         obj = keep.Keep()
-        self.assertEqual(obj.say_hello(), "Hello World", "Should return hello world.")
+        self.assertEqual(
+            obj.say_hello(" World"), "Hello World", "Should return hello world."
+        )
+
+    def test_add(self):
+        """Tests the add two numbers function
+        """
+        obj = keep.Keep()
+        self.assertEqual(
+            obj.add_numbers(first_number=1, second_number=2), 3, msg="1 + 2 should be 3"
+        )

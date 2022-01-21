cdd725d0183476cb60d7af92d2b8be97dfa754be
sumukshashidhar <sumukuuu@gmail.com>
2022-01-21 11:41:49 -0600

    feat[keep]: add simple class

---
+++ src/keep.py
@@ -0,0 +1,7 @@
+"""Simple module that should pass the pylint testcase for sure."""
+class Keep:
+    def __init__(self) -> None:
+        pass
+    
+    def say_hello(self):
+        return "Hello World"

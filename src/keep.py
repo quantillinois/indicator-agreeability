a12e97b6a4aa89e65e817fca50747d8b4ef4cdcc
Sushruth Booma <sushruth2003@gmail.com>
2022-01-31 23:21:09 -0600

    implemented EMA + unit test case

--- src/keep.py
+++ src/keep.py
@@ -1,6 +1,6 @@
 """Simple module that should pass the pylint testcase for sure."""
 
-
+import pandas as pd
 class Keep:
     """Simple test class to test out basic Git workflows."""
 
@@ -32,3 +32,32 @@ class Keep:
         self.first_number = first_number
         self.second_number = second_number
         return first_number + second_number
+    '''
+    Input - 
+    price_array = [5.093202617298384, 5.1186766373255494, 5.173893746495927, 5.295009073341559, 5.399191447443256, 5.351825535182657, 5.408024435253786, 5.405247024879783, 5.405073037988975, 5.432671259949622]
+    smoothing = 2 
+    window_size = 20
+
+    Output = 
+    ema array
+    '''
+    def exponential_moving_average(self, price_array, smoothing, window_size):
+        #df = pd.DataFrame(price_array, columns=['price'])
+        #print(df)
+        ema = []
+        ema_value = 0
+        for price in price_array:
+            if ema_value == 0:
+                ema_value = price
+            else:
+                ema_value= smoothing*price+(1-smoothing)*ema_value
+            ema.append(ema_value)
+        #print(ema)
+        return ema
+
+
+
+
+
+
+

d2e1a1039eba7a36ed358613ed5d0041bef519fc
Restyled.io <commits@restyled.io>
2022-02-02 19:49:32 +0000

    Restyled by autopep8

--- src/SimpleMovingAverage.py
+++ src/SimpleMovingAverage.py
@@ -1,7 +1,7 @@
 import numpy as np
 import numpy as np
 import matplotlib.pyplot as plt
- 
+
 # generating time series price data
 mu = 0.001
 sigma = 0.01
@@ -9,26 +9,26 @@ start_price = 5
 np.random.seed(0)
 returns = np.random.normal(loc=mu, scale=sigma, size=100)
 price = start_price*(1+returns).cumprod()
- 
-# Simple Moving Averages 
+
+# Simple Moving Averages
 window_size = 3
 i = 0
 # Initialize an empty list to store moving averages
 moving_averages = []
- 
-#consider every window of size 3
+
+# consider every window of size 3
 while i < len(price) - window_size + 1:
     # Calculate the average of current window
-   window_average = round(np.sum(price[
-     i:i+window_size]) / window_size, 2)
-    
-   # Store the average of current
-   # window in moving average list
-   moving_averages.append(window_average)
-    
-   # Shift window to right by one position
-   i += 1
- 
+    window_average = round(np.sum(price[
+        i:i+window_size]) / window_size, 2)
+
+    # Store the average of current
+    # window in moving average list
+    moving_averages.append(window_average)
+
+    # Shift window to right by one position
+    i += 1
+
 # Plotting the SMA graph
 plt.plot(price)
 plt.plot(moving_averages)
